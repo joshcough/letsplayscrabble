2513fe0f49b039b98ff55c19cb85dc6804fbd7f4
Josh Cough <joshcough@gmail.com>
2024-10-31 00:28:45 -0400

    forgot to add these.

---
+++ backend/src/models/tournament.js
@@ -0,0 +1,14 @@
+// backend/models/tournament.js
+const mongoose = require('mongoose');
+
+const tournamentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  city: { type: String, required: true },
+  year: { type: Number, required: true },
+  lexicon: { type: String, required: true },
+  longFormName: { type: String, required: true },
+  dataUrl: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Tournament', tournamentSchema);
